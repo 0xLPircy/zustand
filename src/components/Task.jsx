0b42171016ed523d26d62cd7aee5169acab4d7a6
L-Pircy <taveesha.agarwal@gmail.com>
2023-05-30 17:04:39 +0530

    components and basic styling done

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import "./task.css";
 
+const STATUS = 'PLANNED';
 
 const Task = ({ title }) => {
     return (
-        <div className='task'>{title}</div>
+        <div className='task'>
+            <div>{title}</div>
+            <div className='bottomWrapper'>
+                <div></div>
+                <div className={classNames('status', STATUS)}>{STATUS}</div>
+            </div>
+        </div>
     )
 }
 

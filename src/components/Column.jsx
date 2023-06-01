b5ff2511e53ff759542262cd317b11198a3ab3bb
L-Pircy <taveesha.agarwal@gmail.com>
2023-06-01 21:02:57 +0530

    adding task, put addTask func in the store, used open to dispkay and not the form

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./column.css";
 import Task from "./Task.jsx";
 import { useStore } from '../store.js';
 
 const Column = ({ state }) => {
+    const [text, setText] = useState('');
+    const [open, setOpen] = useState(false);
     const tasks = useStore((store) => store.tasks.filter((task) => task.state === state));
-
+    const addTask = useStore(store => store.addTask);
     return (
         <div className="column">
             <div className="titleWrapper">
                 <p>{state}</p>
-                <button>Add</button>
+                <button onClick={() => setOpen(true)}>Add</button>
             </div>
             <p>{state}</p>
             {tasks.map((task) => <Task title={task.title} key={task.title} />)}
+            {open && <div className="modal">
+                <div className="modalContent">
+                    <input type="text" onChange={(e) => setText(e.target.value)} value={text} />
+                    <button onClick={() => {
+                        addTask(text, state);
+                        setText('');
+                        setOpen(false);
+                    }}>Submit</button>
+                </div>
+            </div>}
         </div>
     )
 }

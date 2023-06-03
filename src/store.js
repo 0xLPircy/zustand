723ba3f83263bbadd8337af3fd20d660da4bee48
L-Pircy <taveesha.agarwal@gmail.com>
2023-06-03 22:38:40 +0530

    using middleware and redux dev tools for store params

--- src/store.js
+++ src/store.js
@@ -1,14 +1,15 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
 const store = (set) => ({
     tasks: [{ title: 'TestTask', state: 'ONGOING' }],
     draggedTask: null,
     addTask: (title, state) =>
-        set((store) => ({ tasks: [...store.tasks, { title, state }] })),
+        set((store) => ({ tasks: [...store.tasks, { title, state }] }), false, "addTask"),
     deleteTask: (title, state) =>
         set((store) => ({ tasks: store.tasks.filter((task) => task.title != title) })),
     setDraggedTask: (title) => set({ draggedTask: title }),
     moveTask: (title, state) => set((store) => ({ tasks: store.tasks.map(task => task.title === title ? { title, state } : task) }))
 });
 
-export const useStore = create(store);
+export const useStore = create(devtools(store));
